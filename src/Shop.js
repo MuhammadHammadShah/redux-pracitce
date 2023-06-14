b265ffdedf0c4cf04ada64cab6a6afa46643e1ca
MuhammadHammadShah <hammad4455shah5544@gmail.com>
2023-06-14 11:03:10 -0700

    redux practicing  navbar and shop.js is also creeated

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div>
+      <h2>Adidas shoes 50$</h2>
+      <button className="btn btn-primary mx-2">+</button>
+      Add This Item to Cart
+      <button className="btn btn-primary mx-2">-</button>
+    </div>
+  );
+};
+
+export default Shop;
